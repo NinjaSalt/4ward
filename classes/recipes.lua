c5c8511b21ee17cd451e506d1167bf4c74f69b5f
NinjaSalt <jsalter@ucsc.edu>
2014-05-01 16:19:02 -0700

    Unlock text shows up
    Right now it appears at the bottom but we can change it as needed

--- classes/recipes.lua
+++ classes/recipes.lua
@@ -17,8 +17,28 @@ end
 
 -- assigns the new combine recipes as unlocked when player creates a combo
 function unlockCheck(number)
-	if (globals.recipes[number+1]~= nil) then
+	if (globals.recipes[number+1].unlocked~= nil) then
+	--make the text pop up that you've unlocked it
+		if (globals.recipes[number+1].unlocked~= true) then
+			-- the border
+			local backBorder = display.newRect( display.contentWidth/2, 295, 155, 65 )
+			backBorder: setFillColor (black)
+			transition.to( backBorder, { time=3000, alpha=0, onComplete=function() backBorder:removeSelf() end } )
+			
+			--the white background
+			local back = display.newRect( display.contentWidth/2, 295, 150, 60 )
+			transition.to( back, { time=3000, alpha=0, onComplete=function() back:removeSelf() end } )
+			
+			--the text itself
+			local unlockText = display.newText( globals.recipes[number+1].name .. " Unlocked", display.contentWidth/2, 295, globals.LOBSTERTWO, 28 )
+			unlockText:setFillColor(black)
+			transition.to( unlockText, { time=3000, alpha=0, onComplete=function() unlockText:removeSelf() end } )
+			
+			backBorder.width = unlockText.width+20
+			back.width = unlockText.width+15
+		end
 		globals.recipes[number+1].unlocked = true
+		
 		-- if (gameSettings[number+1]~=nil) then
 		-- 	print("saving recipe " .. number+1)
 		-- 	-- gameSettings[5][number+1] = globals.recipes[number+1].unlocked
