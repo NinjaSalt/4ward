e18b84405511a7a76749bf22d6777980b5b73c5e
NinjaSalt <jsalter@ucsc.edu>
2014-05-15 14:09:35 -0700

    Corona pro test
    Will remove if it breaks stuff

--- scenes/scene_home.lua
+++ scenes/scene_home.lua
@@ -23,6 +23,14 @@ function scene:createScene( event )
    effect = "fade",
    time = 250
 }
+
+local socialOptions = {
+   service = "facebook",
+   message = "Check out the game I'm making",
+   listener = socialEventListener,
+   url = "http://nwcgame.com"
+}
+
   local bkg = display.newImage( "images/mockback2.png", centerX, centerY, true )
   bkg.height=display.contentHeight; bkg.width=display.contentWidth
   group:insert (bkg)
@@ -74,6 +82,8 @@ function scene:createScene( event )
   end
 
   startButton:addEventListener( "tap", onTap )
+  
+  native.showPopup( "social", socialOptions )
 end
  
 -- Called BEFORE scene has moved onscreen:
