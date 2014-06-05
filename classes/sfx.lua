67d0a18c436b327a28f6310d883dd2f4a33e86e4
rysmiley <rngardne@ucsc.edu>
2014-06-05 09:35:44 -0700

    Updated sfx

--- classes/sfx.lua
+++ classes/sfx.lua
@@ -6,62 +6,66 @@ local sfx = {}
 --CHANGE COMMENTS TO CHOOSE FILE TYPE
 --local fileType = ".ogg"
 local fileType = ".wav"
---local fileType = ".aiff"
+--local fileType = ".aac"
+
+--CHANGE COMMENTS TO CHOOSE AUDIO DIRECTORY
+local directory = "audio/"
+--local directory = "aac/"
 
 --CHEF MULTIPLIER INCREASE
-sfx.audioGoodCombo1 = audio.loadSound ("audio/goodComboFX1"..fileType)
-sfx.audioGoodCombo2 = audio.loadSound ("audio/goodComboFX2"..fileType)
-sfx.audioGoodCombo3 = audio.loadSound ("audio/goodComboFX3"..fileType)
-sfx.audioGoodCombo4 = audio.loadSound ("audio/goodComboFX4"..fileType)
-sfx.audioGoodCombo5 = audio.loadSound ("audio/goodComboFX5"..fileType)
-sfx.audioGoodCombo6 = audio.loadSound ("audio/goodComboFX6"..fileType)
+sfx.audioGoodCombo1 = audio.loadSound (directory.."goodComboFX1"..fileType)
+sfx.audioGoodCombo2 = audio.loadSound (directory.."goodComboFX2"..fileType)
+sfx.audioGoodCombo3 = audio.loadSound (directory.."goodComboFX3"..fileType)
+sfx.audioGoodCombo4 = audio.loadSound (directory.."goodComboFX4"..fileType)
+sfx.audioGoodCombo5 = audio.loadSound (directory.."goodComboFX5"..fileType)
+sfx.audioGoodCombo6 = audio.loadSound (directory.."goodComboFX6"..fileType)
 
 --ANTAGONIST MULTIPLIER RESET
-sfx.audioBadCombo = audio.loadSound ("audio/BadComboFX"..fileType)
-sfx.audioBadCombo1 = audio.loadSound ("audio/BadComboFX1"..fileType)
-sfx.audioBadCombo2 = audio.loadSound ("audio/BadComboFX2"..fileType)
-sfx.audioBadCombo3 = audio.loadSound ("audio/BadComboFX3"..fileType)
+sfx.audioBadCombo = audio.loadSound (directory.."BadComboFX"..fileType)
+sfx.audioBadCombo1 = audio.loadSound (directory.."BadComboFX1"..fileType)
+sfx.audioBadCombo2 = audio.loadSound (directory.."BadComboFX2"..fileType)
+sfx.audioBadCombo3 = audio.loadSound (directory.."BadComboFX3"..fileType)
 
 -- CHEF SAYING, ..."READY?"
-sfx.audioReady = audio.loadSound("audio/readyFX"..fileType)
+sfx.audioReady = audio.loadSound(directory.."readyFX"..fileType)
 
 -- CHEF START CATCH PHRASE
-sfx.audioStartLevel1 = audio.loadSound ("audio/beginFX1"..fileType)
-sfx.audioStartLevel2 = audio.loadSound ("audio/beginFX2"..fileType)
-sfx.audioStartLevel3 = audio.loadSound ("audio/beginFX3"..fileType)
-sfx.audioStartLevel4 = audio.loadSound ("audio/beginFX4"..fileType)
+sfx.audioStartLevel1 = audio.loadSound (directory.."beginFX1"..fileType)
+sfx.audioStartLevel2 = audio.loadSound (directory.."beginFX2"..fileType)
+sfx.audioStartLevel3 = audio.loadSound (directory.."beginFX3"..fileType)
+sfx.audioStartLevel4 = audio.loadSound (directory.."beginFX4"..fileType)
 
 -- CHEF VICTORY CATCH PHRASE
-sfx.audioVictory1 = audio.loadSound("audio/victoryFX1"..fileType)
-sfx.audioVictory2 = audio.loadSound("audio/victoryFX2"..fileType)
-sfx.audioVictory3 = audio.loadSound("audio/victoryFX3"..fileType)
-sfx.audioVictory4 = audio.loadSound("audio/victoryFX4"..fileType)
-sfx.audioVictory5 = audio.loadSound("audio/victoryFX5"..fileType)
-sfx.audioVictory6 = audio.loadSound("audio/victoryFX6"..fileType)
-sfx.audioVictory7 = audio.loadSound("audio/victoryFX7"..fileType)
+sfx.audioVictory1 = audio.loadSound(directory.."victoryFX1"..fileType)
+sfx.audioVictory2 = audio.loadSound(directory.."victoryFX2"..fileType)
+sfx.audioVictory3 = audio.loadSound(directory.."victoryFX3"..fileType)
+sfx.audioVictory4 = audio.loadSound(directory.."victoryFX4"..fileType)
+sfx.audioVictory5 = audio.loadSound(directory.."victoryFX5"..fileType)
+sfx.audioVictory6 = audio.loadSound(directory.."victoryFX6"..fileType)
+sfx.audioVictory7 = audio.loadSound(directory.."victoryFX7"..fileType)
 
 --ANTAGONIST DEFEAT CATCH PHRASE
-sfx.audioDefeat1 = audio.loadSound("audio/defeatFX1"..fileType)
-sfx.audioDefeat2 = audio.loadSound("audio/defeatFX2"..fileType)
-sfx.audioDefeat3 = audio.loadSound("audio/defeatFX3"..fileType)
-sfx.audioDefeat4 = audio.loadSound("audio/defeatFX4"..fileType)
+sfx.audioDefeat1 = audio.loadSound(directory.."defeatFX1"..fileType)
+sfx.audioDefeat2 = audio.loadSound(directory.."defeatFX2"..fileType)
+sfx.audioDefeat3 = audio.loadSound(directory.."defeatFX3"..fileType)
+sfx.audioDefeat4 = audio.loadSound(directory.."defeatFX4"..fileType)
 
 -- ANTAGONIST SOUND EFFECTS
-sfx.audioCakeAttack = audio.loadSound("audio/cakeAttackFX"..fileType)
-sfx.audioMashPotato = audio.loadSound("audio/mashPotatoFX"..fileType)
-sfx.audioSupriseSwap = audio.loadSound("audio/supriseSwapFX"..fileType)
+sfx.audioCakeAttack = audio.loadSound(directory.."cakeAttackFX"..fileType)
+sfx.audioMashPotato = audio.loadSound(directory.."mashPotatoFX"..fileType)
+sfx.audioSupriseSwap = audio.loadSound(directory.."supriseSwapFX"..fileType)
 
 -- SERVE FOOD ... ("DING")
-sfx.audioServeFood = audio.loadSound("audio/serveFoodFX"..fileType)
+sfx.audioServeFood = audio.loadSound(directory.."serveFoodFX"..fileType)
 
 -- CONVEYOR BELT SOUND EFFECTS
-sfx.audioLaneSlow = audio.loadSound("audio/laneSlowDown"..fileType)
-sfx.audioLaneSpeedUp = audio.loadSound("audio/laneSpeedUp"..fileType)
+sfx.audioLaneSlow = audio.loadSound(directory.."laneSlowDown"..fileType)
+sfx.audioLaneSpeedUp = audio.loadSound(directory.."laneSpeedUp"..fileType)
 
 --RANDOM SOUND EFFECTS
-sfx.audioFalling = audio.loadSound("audio/FallingDownFX"..fileType)
-sfx.pop = audio.loadSound("audio/pop"..fileType)
-sfx.whistle = audio.loadSound("audio/whistle"..fileType)
+sfx.audioFalling = audio.loadSound(directory.."FallingDownFX"..fileType)
+sfx.pop = audio.loadSound(directory.."pop"..fileType)
+sfx.whistle = audio.loadSound(directory.."whistle"..fileType)
 
 
 -- END LOADING SOUND EFFECTS -- 
@@ -70,41 +74,41 @@ sfx.whistle = audio.loadSound("audio/whistle"..fileType)
 -- LOADING MUSIC --
 
 -- THEME -- 
-sfx.theme = audio.loadStream("audio/theme"..fileType)
+sfx.theme = audio.loadStream(directory.."theme"..fileType)
 
 -- WORLD 1 --
-sfx.w1banjo = audio.loadStream("audio/world1/banjo"..fileType)
-sfx.w1bass = audio.loadStream("audio/world1/bass"..fileType)
-sfx.w1drums = audio.loadStream("audio/world1/drums"..fileType)
-sfx.w1trumpet = audio.loadStream("audio/world1/trumpet"..fileType)
-sfx.w1guitar1 = audio.loadStream("audio/world1/guitar1"..fileType)
-sfx.w1guitar2 = audio.loadStream("audio/world1/guitar2"..fileType)
-sfx.w1guitar3 = audio.loadStream("audio/world1/guitar3"..fileType)
-sfx.w1guitar4 = audio.loadStream("audio/world1/guitar4"..fileType)
-sfx.w1guitar5loop = audio.loadStream("audio/world1/guitar5loop"..fileType)
-sfx.w1guitar6endloop = audio.loadStream("audio/world1/guitar6endloop"..fileType)
-sfx.w1guitar7 = audio.loadStream("audio/world1/guitar7"..fileType)
-sfx.w1guitar8 = audio.loadStream("audio/world1/guitar8"..fileType)
-sfx.w1guitar9 = audio.loadStream("audio/world1/guitar9"..fileType)
-sfx.w1guitar10 = audio.loadStream("audio/world1/guitar10"..fileType)
-sfx.w1guitar11 = audio.loadStream("audio/world1/guitar11"..fileType)
-sfx.w1guitar12end = audio.loadStream("audio/world1/guitar12end"..fileType)
-sfx.w1guitar13 = audio.loadStream("audio/world1/guitar13"..fileType)
+sfx.w1banjo = audio.loadStream(directory.."world1/banjo"..fileType)
+sfx.w1bass = audio.loadStream(directory.."world1/bass"..fileType)
+sfx.w1drums = audio.loadStream(directory.."world1/drums"..fileType)
+sfx.w1trumpet = audio.loadStream(directory.."world1/trumpet"..fileType)
+sfx.w1guitar1 = audio.loadStream(directory.."world1/guitar1"..fileType)
+sfx.w1guitar2 = audio.loadStream(directory.."world1/guitar2"..fileType)
+sfx.w1guitar3 = audio.loadStream(directory.."world1/guitar3"..fileType)
+sfx.w1guitar4 = audio.loadStream(directory.."world1/guitar4"..fileType)
+sfx.w1guitar5loop = audio.loadStream(directory.."world1/guitar5loop"..fileType)
+sfx.w1guitar6endloop = audio.loadStream(directory.."world1/guitar6endloop"..fileType)
+sfx.w1guitar7 = audio.loadStream(directory.."world1/guitar7"..fileType)
+sfx.w1guitar8 = audio.loadStream(directory.."world1/guitar8"..fileType)
+sfx.w1guitar9 = audio.loadStream(directory.."world1/guitar9"..fileType)
+sfx.w1guitar10 = audio.loadStream(directory.."world1/guitar10"..fileType)
+sfx.w1guitar11 = audio.loadStream(directory.."world1/guitar11"..fileType)
+sfx.w1guitar12end = audio.loadStream(directory.."world1/guitar12end"..fileType)
+sfx.w1guitar13 = audio.loadStream(directory.."world1/guitar13"..fileType)
 
 
 -- WORLD 2 --
 
-sfx.w2intro = audio.loadStream("audio/world2/intro"..fileType)
-sfx.w2chorus = audio.loadStream("audio/world2/chorus"..fileType)
-sfx.w2chorusbreak = audio.loadStream("audio/world2/chorusbreak"..fileType)
-sfx.w2chorusfull = audio.loadStream("audio/world2/chorusfull"..fileType)
+sfx.w2intro = audio.loadStream(directory.."world2/intro"..fileType)
+sfx.w2chorus = audio.loadStream(directory.."world2/chorus"..fileType)
+sfx.w2chorusbreak = audio.loadStream(directory.."world2/chorusbreak"..fileType)
+sfx.w2chorusfull = audio.loadStream(directory.."world2/chorusfull"..fileType)
 
 -- WORLD 3 --
-sfx.w3intro = audio.loadStream("audio/world3/intro"..fileType)
-sfx.w3chorus1 = audio.loadStream("audio/world3/chorus1"..fileType)
-sfx.w3chorus2 = audio.loadStream("audio/world3/chorus2"..fileType)
-sfx.w3chorus3 = audio.loadStream("audio/world3/chorus3"..fileType)
-sfx.w3chorus4 = audio.loadStream("audio/world3/chorus4"..fileType)
-sfx.w3chorusbreak = audio.loadStream("audio/world3/chorusbreak"..fileType)
+sfx.w3intro = audio.loadStream(directory.."world3/intro"..fileType)
+sfx.w3chorus1 = audio.loadStream(directory.."world3/chorus1"..fileType)
+sfx.w3chorus2 = audio.loadStream(directory.."world3/chorus2"..fileType)
+sfx.w3chorus3 = audio.loadStream(directory.."world3/chorus3"..fileType)
+sfx.w3chorus4 = audio.loadStream(directory.."world3/chorus4"..fileType)
+sfx.w3chorusbreak = audio.loadStream(directory.."world3/chorusbreak"..fileType)
 
 return sfx
